21a5a4f4bc9685028c0f280c51b942450cd415fa
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-06-19 15:56:52 +0200

    start whit class Prodotto and Main

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,15 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+public class Prodotto {
+
+	private int code;
+	private String name;
+	private String description;
+	private double price;
+	private double tax;
+	
+	//Costruttore
+	
+}
